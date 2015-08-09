6665b75f29c86e20b7bd7bc9f2c94e0ad149b82c
John Elash <elash@telus.net>
2015-08-09 15:21:57 -0700

    Create initial repository

---
+++ Wordpress Training/building themes from scratch using underscores/Ex_Files_WP_Underscores/Ex_Files_WP_Underscores/Exercise Files/startpoints/Chapter-08/02/content-single.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package my-simone
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+            
+            <?php
+                /* translators: used between list items, there is a space after the comma */
+                $category_list = get_the_category_list( __( ', ', 'my-simone' ) );
+
+                if ( my_simone_categorized_blog() ) {
+                    echo '<div class="category-list">' . $category_list . '</div>';
+                }
+            ?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+
+		<div class="entry-meta">
+			<?php my_simone_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'my-simone' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php
+                    echo get_the_tag_list( '<ul><li><i class="fa fa-tag"></i>', '</li><li><i class="fa fa-tag"></i>', '</li></ul>' );
+                ?>
+
+		<?php edit_post_link( __( 'Edit', 'my-simone' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
---
+++ Wordpress Training/building themes from scratch using underscores/Ex_Files_WP_Underscores/Ex_Files_WP_Underscores/Exercise Files/startpoints/Chapter-11/04/content.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package my-simone
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="index-box">
+	<header class="entry-header">
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php my_simone_posted_on(); ?>
+                        <?php 
+                            if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) { 
+                                echo '<span class="comments-link">';
+                                comments_popup_link( __( 'Leave a comment', 'my-simone' ), __( '1 Comment', 'my-simone' ), __( '% Comments', 'my-simone' ) );
+                                echo '</span>';
+                            }
+                        ?>
+                        <?php edit_post_link( __( 'Edit', 'my-simone' ), '<span class="edit-link">', '</span>' ); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+	
+	<div class="entry-content">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+	
+         </footer><!-- .entry-footer -->
+    </div><!-- .index-box -->
+</article><!-- #post-## -->
