f4f8018760c10ba00e272ba67529db08b8a72c53
John Elash <elash@telus.net>
2015-08-09 17:13:05 -0700

    working on course
    difficult work...getting to learn a lot but slowly

--- wp_simone/themes/my-simone/content/functions-jme.php
+++ wp_simone/themes/my-simone/content/functions-jme.php
@@ -1,16 +1,11 @@
 <?php
 /**
- * my-simone functions and definitions
+ * my-simone functions and definitions.
+ *
+ * @link https://codex.wordpress.org/Functions_File_Explained
  *
  * @package my-simone
  */
-
-/**
- * Set the content width based on the theme's design and stylesheet.
- */
-if ( ! isset( $content_width ) ) {
-	$content_width = 600; /* pixels */
-}
 
 if ( ! function_exists( 'my_simone_setup' ) ) :
 /**
@@ -21,7 +16,6 @@ if ( ! function_exists( 'my_simone_setup' ) ) :
  * as indicating support for post thumbnails.
  */
 function my_simone_setup() {
-
 	/*
 	 * Make theme available for translation.
 	 * Translations can be filed in the /languages/ directory.
@@ -34,49 +28,84 @@ function my_simone_setup() {
 	add_theme_support( 'automatic-feed-links' );
 
 	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'my-simone' ),
+		'primary' => esc_html__( 'Primary Menu', 'my-simone' ),
 	) );
 
-	// Enable support for Post Formats.
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
 
-	// Setup the WordPress core custom background feature.
+	/*
+	 * Enable support for Post Formats.
+	 * See https://developer.wordpress.org/themes/functionality/post-formats/
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside',
+		'image',
+		'video',
+		'quote',
+		'link',
+	) );
+
+	// Set up the WordPress core custom background feature.
 //	add_theme_support( 'custom-background', apply_filters( 'my_simone_custom_background_args', array(
 //		'default-color' => 'ffffff',
 //		'default-image' => '',
 //	) ) );
-
-	// Enable support for HTML5 markup.
-	add_theme_support( 'html5', array(
-		'comment-list',
-		'search-form',
-		'comment-form',
-		'gallery',
-                'caption',    
-	) );
 }
 endif; // my_simone_setup
 add_action( 'after_setup_theme', 'my_simone_setup' );
 
 /**
- * Register widgetized area and update sidebar with default widgets.
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function my_simone_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'my_simone_content_width', 600 );
+}
+add_action( 'after_setup_theme', 'my_simone_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function my_simone_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'my-simone' ),
+		'name'          => esc_html__( 'Sidebar', 'my-simone' ),
 		'id'            => 'sidebar-1',
+		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
 	) );
 }
 add_action( 'widgets_init', 'my_simone_widgets_init' );
